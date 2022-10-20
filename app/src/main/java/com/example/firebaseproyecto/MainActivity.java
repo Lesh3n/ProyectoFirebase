82ae2da6974814324ab83debae896e6cd38d6358
AndresicK <andytimy@gmail.com>
2022-10-20 14:53:16 -0300

    Se agregó btn Agregar, modificacion de builgradle y cambios en clase Main Clase 1 Firebase

--- app/src/main/java/com/example/firebaseproyecto/MainActivity.java
+++ app/src/main/java/com/example/firebaseproyecto/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.firebaseproyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnAgregar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAgregar = (Button) findViewById(R.id.btnAgregar);
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Presionaste el botón agregar", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
