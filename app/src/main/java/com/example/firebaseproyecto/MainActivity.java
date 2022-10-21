c523f9c6e4575b834f1298f1eaf2a10474161fd3
AndresicK <andytimy@gmail.com>
2022-10-21 09:32:23 -0300

    Clase 2 Función insertar para ingresar datos a base de datos en tiempo real (firebase)

--- app/src/main/java/com/example/firebaseproyecto/MainActivity.java
+++ app/src/main/java/com/example/firebaseproyecto/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
     Button btnAgregar;
     @Override
@@ -18,7 +21,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Presionaste el botón agregar", Toast.LENGTH_SHORT).show();
+                insertar();
             }
         });
+    }
+    public void insertar() {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Alumno").child("r1"); // Busca una clave que se llama alumno y crea un hijo de r1 para guardar todos los datos dentro.
+
+        myRef.child("Nombre").setValue("Andrés"); // A r1 agréguele la clave Nombre y valor Andrés
+        myRef.child("Apellido").setValue("Ibáñez");  // A r1 agréguele la clave Apellido y valor Ibáñez
+
+
+        myRef.setValue("Hello, World!");
     }
 }
